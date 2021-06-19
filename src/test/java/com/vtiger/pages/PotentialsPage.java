3d81a94feb99aa61cd650fc312a8899fc1aba436
ravindrachougule66 <ravindrachougule66@gmail.com>
2021-06-20 04:19:11 +0530

    PotentialPage=CreateNewPotential method

--- src/test/java/com/vtiger/pages/PotentialsPage.java
+++ src/test/java/com/vtiger/pages/PotentialsPage.java
@@ -1,6 +1,8 @@
 package com.vtiger.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import com.vtiger.lib.commonfunctions;
 
@@ -14,6 +16,48 @@ public class PotentialsPage extends HeaderPage{
 			super(driver);
 			this.driver = driver;
 			PageFactory.initElements(driver, this);
+		}
+		
+		@FindBy(name="potentialname")
+		WebElement potentialname;
+		
+		@FindBy(name="amount")
+		WebElement amount;
+		
+		@FindBy(name="closingdate")
+		WebElement closingdate;
+		
+		@FindBy(name="opportunity_type")
+		WebElement opportunity_type;
+		
+		@FindBy(name="leadsource")
+		WebElement leadsource;
+		
+		@FindBy(name="nextstep")
+		WebElement nextstep;
+		
+		@FindBy(name="sales_stage")
+		WebElement sales_stage;
+		
+		@FindBy(name="btn1")
+		WebElement btn1;
+		
+		@FindBy(linkText="vtiger")
+		WebElement vtiger;
+		
+		public void CreateNewPotential(String pname,String amt,String cdate,String otype,String nstep,String lsource,String sstage)
+		{
+			ClickNewPotential();
+			cm.EnterValue(driver,potentialname,pname,"Potential name");
+			cm.EnterValue(driver,amount,amt,"Amount");
+			cm.ClickElement(driver,btn1,"Change button");
+			cm.ActionOnPopUpWindows(driver,vtiger,"vtiger");
+			cm.EnterValue(driver,closingdate,cdate,"Closing date");
+			cm.SelectByValue(driver, opportunity_type, otype, "Business type");
+			cm.EnterValue(driver, nextstep, nstep, "Next step");
+			cm.SelectByValue(driver, leadsource, lsource, "Lead source");
+			cm.SelectByValue(driver, sales_stage, sstage, "Sales stage");
+			ClickSaveButton();
 		}
 
 }
