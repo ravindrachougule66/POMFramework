e0a1b1b8f8e4ac66e867adc7880a9cd5e69387ad
ravindrachougule66 <ravindrachougule66@gmail.com>
2021-06-20 22:03:45 +0530

    updated

--- src/test/java/com/vtiger/tests/ProductsTests.java
+++ src/test/java/com/vtiger/tests/ProductsTests.java
@@ -17,7 +17,7 @@ public class ProductsTests extends BaseTests{
 		pp.CreateNewPriceBook("Internet Sales","New Internet sales price book added");
 		extent.flush();
 	}
-	//@Test (priority=2)
+	@Test (priority=2)
 	public void verifyCreateNewVendor_TC03()
 	{
 		logger = extent.createTest("Verify Create New Vendor_TC03");
@@ -25,18 +25,18 @@ public class ProductsTests extends BaseTests{
 		pp.CreateNewVendor("Mark","(095)859-7757","mark@company.com","www.vtigercrm.com","306-Internet Sales","999 Baker Way","San Jose","CA","77899","USA");
 		extent.flush();
 	}
-	//@Test (priority=1)
-	public void verifyCreateNewProduct_TC01()
+	@Test (priority=1)
+	public void verifyCreateNewProduct_TC02()
 	{
-		logger = extent.createTest("Verify Create New Product_TC01");
+		logger = extent.createTest("Verify Create New Product_TC02");
 		pp = new ProductsPage(driver);
-		pp.CreateNewProduct("Vtiger 15 Users Pack", "004", "0.000",2);
+		pp.CreateNewProduct("Vtiger 15 Users Pack","004","MetBeat Corp","5");
 		extent.flush();
 	}
-	//@Test (priority=0)
-	public void verifySubMenusOnProductsPage_TC02()
+	@Test (priority=0)
+	public void verifySubMenusOnProductsPage_TC01()
 	{
-		logger = extent.createTest("verify New Event_TC02");
+		logger = extent.createTest("Verify SubMenus On Products Page_TC01");
 		pp = new ProductsPage(driver);
 		pp.ClickProducts();
 		pp.IssubMenuProductsDisplay();
