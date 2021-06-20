0c20a4f5742176ca0d315b5dc114caf4d1b7eecf
ravindrachougule66 <ravindrachougule66@gmail.com>
2021-06-20 21:25:34 +0530

    methods added

--- src/test/java/com/vtiger/pages/ProductsPage.java
+++ src/test/java/com/vtiger/pages/ProductsPage.java
@@ -22,32 +22,72 @@ public class ProductsPage extends HeaderPage{
 	
 	@FindBy(linkText="Products")
 	WebElement Products;
-	
 	@FindBy(linkText="Vendors")
 	WebElement Vendors;
-	
 	@FindBy(linkText="Price Books")
 	WebElement PriceBooks;
-	
 	@FindBy(name="productname")
 	WebElement productname;
-	
 	@FindBy(name="productcode")
 	WebElement productcode;
-	
 	@FindBy(name="discontinued")
 	WebElement discontinued;
-	
 	@FindBy(name="manufacturer")
 	WebElement manufacturer;
-	
 	@FindBy(name="commissionrate")
 	WebElement commissionrate;
-	
 	@FindBy(xpath="(//input[@name='button'])[4]")
 	WebElement save;
+	@FindBy(name="vendorname")
+	WebElement vendorname;
+	@FindBy(name="phone")
+	WebElement phone;
+	@FindBy(name="email")
+	WebElement email;
+	@FindBy(name="website")
+	WebElement website;
+	@FindBy(name="glacct")
+	WebElement glacct;
+	@FindBy(name="treet")
+	WebElement treet;
+	@FindBy(xpath="//input[@name='city']")
+	WebElement city;
+	@FindBy(name="state")
+	WebElement state;
+	@FindBy(name="postalcode")
+	WebElement postalcode;
+	@FindBy(name="country")
+	WebElement country;
+	@FindBy(name="bookname")
+	WebElement bookname;
+	@FindBy(name="description")
+	WebElement description;
+	@FindBy(name="active")
+	WebElement active;
 	
-	
+	public void CreateNewPriceBook(String bname,String des)
+	{
+		ClickNewPriceBook();
+		cm.EnterValue(driver,bookname,bname,"Price Book name");
+		cm.ClickElement(driver,active,"Active checkbox");
+		cm.EnterValue(driver,description,des,"Description");
+		ClickSaveButton();
+	}
+	public void CreateNewVendor(String vname,String ph,String mail,String web,String gacct,String street,String cit,String stat,String pcode,String count)
+	{
+		ClickNewVendor();
+		cm.EnterValue(driver,vendorname,vname,"Vendor name");
+		cm.EnterValue(driver,phone,ph,"Phone");
+		cm.EnterValue(driver,email,mail,"Email");
+		cm.EnterValue(driver,website,web,"Website");
+		cm.SelectByValue(driver,glacct,gacct,"GL Account");
+		cm.EnterValue(driver,treet,street,"Street");
+		cm.EnterValue(driver,city,cit,"City");
+		cm.EnterValue(driver,state,stat,"State");
+		cm.EnterValue(driver,postalcode,pcode,"Postal code");
+		cm.EnterValue(driver,country,count,"Country");
+		ClickSaveButton();
+	}
 	public void CreateNewProduct(String pname, String Pcode,String crate, int manufact)
 	{
 		ClickNewProduct();
@@ -59,8 +99,7 @@ public class ProductsPage extends HeaderPage{
 		cm.ClickElement(driver, save, "Save button");
 		cm.AlertHandler(driver);
 		driver.switchTo().alert().accept();	
-	}
-	
+	}	
 	public boolean IssubMenuProductsDisplay()
 	{
 		return cm.IsElementPresent(driver, Products, "Products");
