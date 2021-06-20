abd1bbf7ad067d0e770b6c7ac298fb0a894b85e5
ravindrachougule66 <ravindrachougule66@gmail.com>
2021-06-20 07:44:21 +0530

    updated

--- src/test/java/com/vtiger/tests/ProductsTests.java
+++ src/test/java/com/vtiger/tests/ProductsTests.java
@@ -7,12 +7,13 @@ import com.vtiger.pages.ProductsPage;
 
 public class ProductsTests extends BaseTests{
 	
+	public ProductsPage pp;
 	
 	@Test
 	public void verifyCreateNewProduct_TC01()
 	{
 		logger = extent.createTest("Verify Create New Product_TC01");
-		ProductsPage pp = new ProductsPage(driver);
+		pp = new ProductsPage(driver);
 		pp.CreateNewProduct("Vtiger 15 Users Pack", "004", "0.000",2);
 		extent.flush();
 	}
@@ -20,7 +21,7 @@ public class ProductsTests extends BaseTests{
 	public void verifySubMenusOnProductsPage_TC02()
 	{
 		logger = extent.createTest("verify New Event_TC02");
-		ProductsPage pp = new ProductsPage(driver);
+		pp = new ProductsPage(driver);
 		pp.ClickProducts();
 		pp.IssubMenuProductsDisplay();
 		pp.IssubMenuVendorsDisplay();
