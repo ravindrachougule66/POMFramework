7b6bc0f438447a7a33fb5dff3c45adc7f2a7ed9f
ravindrachougule66 <ravindrachougule66@gmail.com>
2021-06-24 09:07:44 +0530

    updated

--- src/test/java/com/vtiger/tests/ContactsTest.java
+++ src/test/java/com/vtiger/tests/ContactsTest.java
@@ -14,7 +14,7 @@ public class ContactsTest extends BaseTest{
 		cp.CreateNewContact("Mr.", "Glen", "Tucker","Public Relations","President","glen.tucker@gmail.com","(508)242-6089", "(601)711-4368", "(918)453-0462", "1990-06-01");
 		extent.flush();
 	}
-	@Test (priority=0)
+	@Test (priority=1)
 	public void ContactSearchAndDelete_TC02() throws InterruptedException
 	{		
 		logger = extent.createTest("verify Contact Search And Delete_TC02");
